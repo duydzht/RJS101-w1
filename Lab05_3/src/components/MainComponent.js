02eed3f1265eefe2f3b271df4615264e78d2ccda
hoang duy duy <hoangduy241199@gmail.com>
2021-08-18 18:42:03 +0700

    SPA part 1

---
+++ Lab05_3/src/components/MainComponent.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import Menu from './MenuComponent';
+import Home from './HomeComponent';
+import Contact from './ContactComponent';
+//import DishDetail from './DishdetailComponent';
+import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { LEADERS } from '../shared/leaders';
+import { PROMOTIONS } from '../shared/promotions';
+
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
+class Main extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            dishes: DISHES,
+            comments: COMMENTS,
+            leaders:LEADERS,
+            promotions: PROMOTIONS,
+        };
+    }
+
+    render() {
+        const HomePage = () => {
+            return (
+                <Home dish = {this.state.dishes.filter((dish) => dish.featured)[0]}
+                    promotion = {this.state.promotions.filter((promo) => promo.featured)[0]}
+                    leader = {this.state.leaders.filter((leader) => leader.featured)[0]}
+                />
+            );
+        }
+        return (
+            <div>
+                <Header />
+                <Switch>
+                    <Route path="/home" component={HomePage} />
+                    {/* HomePage trả về kết quả <Home/> quy định ở trên */}
+                    <Route exact path="/menu" component={() => <Menu dishes = {this.state.dishes}/>} />
+                    <Route exact path="/contactus" component={Contact}/>
+                    <Redirect to="/home" />
+                    {/* khi các link không có đích đến nó sẽ dẫn về home */}
+                </Switch>
+                <Footer/>
+            </div>
+        );
+    }
+}
+
+export default Main;
