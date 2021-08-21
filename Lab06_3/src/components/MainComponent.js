8dfe505cf712d28201a6bfcce1bbc819b55b9e32
hoang duy duy <hoangduy241199@gmail.com>
2021-08-21 17:04:25 +0700

    Uncontroler Form

---
+++ Lab06_3/src/components/MainComponent.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import Menu from './MenuComponent';
+import Home from './HomeComponent';
+import Contact from './ContactComponent';
+import DishDetail from './DishdetailComponent';
+import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { LEADERS } from '../shared/leaders';
+import { PROMOTIONS } from '../shared/promotions';
+import About from './AboutComponent'
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
+class Main extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            dishes: DISHES,
+            comments: COMMENTS,
+            leaders:LEADERS,
+            promotions: PROMOTIONS,
+        };
+    }
+
+    render() {
+        const HomePage = () => {
+            return (
+                <Home dish = {this.state.dishes.filter((dish) => dish.featured)[0]}
+                    promotion = {this.state.promotions.filter((promo) => promo.featured)[0]}
+                    leader = {this.state.leaders.filter((leader) => leader.featured)[0]}
+                />
+            );
+        }
+
+        const DishWithId = ({match}) => {
+            return (
+                <DishDetail dish = {this.state.dishes.filter((dish)=> dish.id === parseInt(match.params.dishId,10))[0]}
+                    comments={this.state.comments.filter((comment)=> comment.dishId === parseInt(match.params.dishId,10))}
+                />
+            );
+        }
+
+        return (
+            <div>
+                <Header />
+                <Switch>
+                    <Route path="/home" component={HomePage} />
+                    {/* HomePage trả về kết quả <Home/> quy định ở trên */}
+                    <Route path="/aboutus" component={() => <About leaders = {this.state.leaders}/>}/>
+                    <Route exact path="/menu" component={() => <Menu dishes = {this.state.dishes}/>} />
+                    {/* exact để ngăn đường dẫn chọn sai vì có 2 /menu */}
+                    <Route path = "/menu/:dishId" component = {DishWithId}/>
+                    <Route exact path="/contactus" component={Contact}/>
+                    <Redirect to="/home" />
+                    {/* khi các link không có đích đến nó sẽ dẫn về home */}
+                </Switch>
+                <Footer/>
+            </div>
+        );
+    }
+}
+
+export default Main;
