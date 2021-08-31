ed68a5123e06564b314312205f1fa755f2822237
hoang duy duy <hoangduy241199@gmail.com>
2021-08-31 16:54:49 +0700

    Redux thunk

---
+++ Lab09_1/src/components/MainComponent.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react';
+import Menu from './MenuComponent';
+import Home from './HomeComponent';
+import Contact from './ContactComponent';
+import DishDetail from './DishdetailComponent';
+import About from './AboutComponent'
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
+import {connect} from 'react-redux';
+import {addComment, fetchDishes } from '../redux/ActionCreators';
+import {actions} from 'react-redux-form';
+
+
+const mapStatetoProps = state => {
+    return {
+        dishes: state.dishes,
+        comments: state.comments,
+        leaders: state.leaders,
+        promotions: state.promotions,
+    }
+}
+
+const mapDispatchToProps = (dispatch) => ({
+    addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment)),
+    fetchDishes: () => { dispatch(fetchDishes())},
+    resetFeedbackForm: ()=> { dispatch(actions.reset('feedback'))}
+})
+
+class Main extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+        this.props.fetchDishes();
+    }
+
+    render() {
+        const HomePage = () => {
+            return(
+                <Home 
+                    dish={this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
+                    dishesLoading={this.props.dishes.isLoading}
+                    dishesErrMess={this.props.dishes.errMess}
+                    promotion={this.props.promotions.filter((promo) => promo.featured)[0]}
+                    leader={this.props.leaders.filter((leader) => leader.featured)[0]}
+                />
+            );
+        }
+
+        const DishWithId = ({match}) => {
+            return(
+                <DishDetail dish={this.props.dishes.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
+                    isLoading={this.props.dishes.isLoading}
+                    errMess={this.props.dishes.errMess}
+                    comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
+                    addComment={this.props.addComment}
+                />
+            );
+        };
+
+        return (
+            <div>
+                <Header />
+                <Switch>
+                    <Route path="/home" component={HomePage} />
+                    {/* HomePage trả về kết quả <Home/> quy định ở trên */}
+                    <Route path="/aboutus" component={() => <About leaders = {this.props.leaders}/>}/>
+                    <Route exact path="/menu" component={() => <Menu dishes = {this.props.dishes}/>} />
+                    {/* exact để ngăn đường dẫn chọn sai vì có 2 /menu */}
+                    <Route path = "/menu/:dishId" component = {DishWithId}/>
+                    <Route exact path="/contactus" component={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} />}/>
+                    <Redirect to="/home" />
+                    {/* khi các link không có đích đến nó sẽ dẫn về home */}
+                </Switch>
+                <Footer/>
+            </div>
+        );
+    }
+}
+
+export default withRouter(connect(mapStatetoProps, mapDispatchToProps)(Main));
