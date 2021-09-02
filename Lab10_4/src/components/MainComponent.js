c8c2b04a28cd19cfcd0868811d952f96aed67725
hoang duy duy <hoangduy241199@gmail.com>
2021-09-02 11:07:52 +0700

    Fetch Post Comment

---
+++ Lab10_4/src/components/MainComponent.js
@@ -0,0 +1,100 @@
+import React, { Component } from 'react';
+import Menu from './MenuComponent';
+import Home from './HomeComponent';
+import Contact from './ContactComponent';
+import DishDetail from './DishdetailComponent';
+import About from './AboutComponent'
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
+import {connect} from 'react-redux';
+import {fetchDishes, fetchComments, fetchPromos, postComment} from '../redux/ActionCreators';
+import {actions} from 'react-redux-form';
+
+
+
+const mapStatetoProps = state => {
+    return {
+        dishes: state.dishes,
+        comments: state.comments,
+        leaders: state.leaders,
+        promotions: state.promotions,
+    }
+}
+
+const mapDispatchToProps = (dispatch) => ({
+    fetchDishes: () => { dispatch(fetchDishes())},
+    resetFeedbackForm: ()=> { dispatch(actions.reset('feedback'))},
+    fetchComments: () => { dispatch(fetchComments())},
+    fetchPromos: () => { dispatch(fetchPromos())},
+    postComment: (dishId, rating, author, comment) => dispatch(postComment(dishId, rating, author, comment)),
+
+
+})
+
+class Main extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+        this.props.fetchDishes();
+        this.props.fetchComments();
+        this.props.fetchPromos();
+    }
+
+    render() {
+        const HomePage = () => {
+            return(
+                <Home
+                    //dishes 
+                    dish={this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
+                    dishesLoading={this.props.dishes.isLoading}
+                    dishesErrMess={this.props.dishes.errMess}
+                    //promotion
+                    promotion={this.props.promotions.promotions.filter((promo) => promo.featured)[0]}
+                    promoLoading={this.props.promotions.isLoading}
+                    promoErrMess={this.props.promotions.errMess}
+                    leader={this.props.leaders.filter((leader) => leader.featured)[0]}
+                    
+                />
+            );
+        }
+
+        const DishWithId = ({match}) => {
+            return(
+                <DishDetail 
+                    //dishes
+                    dish={this.props.dishes.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
+                    isLoading={this.props.dishes.isLoading}
+                    errMess={this.props.dishes.errMess}
+                    //comments
+                    comments={this.props.comments.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
+                    commentsErrMess={this.props.comments.errMess}
+                    postComment={this.props.postComment}
+                />
+            );
+        };
+
+        return (
+            <div>
+                <Header />
+                <Switch>
+                    <Route path="/home" component={HomePage} />
+                    {/* HomePage trả về kết quả <Home/> quy định ở trên */}
+                    <Route path="/aboutus" component={() => <About leaders = {this.props.leaders}/>}/>
+                    <Route exact path="/menu" component={() => <Menu dishes = {this.props.dishes}/>} />
+                    {/* exact để ngăn đường dẫn chọn sai vì có 2 /menu */}
+                    <Route path = "/menu/:dishId" component = {DishWithId}/>
+                    <Route exact path="/contactus" component={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} />}/>
+                    <Redirect to="/home" />
+                    {/* khi các link không có đích đến nó sẽ dẫn về home */}
+                </Switch>
+                <Footer/>
+            </div>
+        );
+    }
+}
+
+export default withRouter(connect(mapStatetoProps, mapDispatchToProps)(Main));
